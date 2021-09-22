f6100df0137c3c6e113e1366fbb61a6ab577d1d8
Martin Jorge Nicolas <nicomartin854@gmail.com>
2021-09-22 12:59:13 -0300

    Agregando eventos Click

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -3,16 +3,43 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas de CounterApp", () => {
-  const wrapper = shallow(<CounterApp />);
+  let wrapper = shallow(<CounterApp />);
+
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
   test("Debe de renderizar el componente correctamente", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
   test("debe imprimir el valor pasado como parametro en los props", () => {
-    const valor = "100";
+    const valor = 100;
+    const wrapper = shallow(<CounterApp value={valor} />);
     const valorParrafo = wrapper.find("h2").text().trim();
-    expect(valorParrafo).toBe(valor);
+    expect(valorParrafo).toBe("100");
   });
 
-  test("", () => {});
+  test("Debe de incrementar en valor en +1", () => {
+    wrapper.find("button").at(0).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("11");
+  });
+
+  test("Debe de restar el valor en -1", () => {
+    wrapper.find("button").at(2).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("9");
+  });
+
+  test("Debe hacer reset el valor", () => {
+    const wrapper = shallow(<CounterApp value={105} />);
+    wrapper.find("button").at(0).simulate("click");
+    wrapper.find("button").at(1).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("105");
+  });
 });
